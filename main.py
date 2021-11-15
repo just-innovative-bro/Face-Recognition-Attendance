8955b50b79f4926ff01cbe37e20db8883913bd74
Mohammed Zameel ali <55248408+Zameel-Byte@users.noreply.github.com>
2021-11-15 17:40:02 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+import enroll,spreadsheet,emailing,recognition
+
+
+#recognition.load_facial_encodings_and_names_from_memory()
+
+#spreadsheet.mark_all_absent()
+
+#recognition.run_recognition()
+
+
+# for enrolling un# the bellow
+#enroll.enroll_via_camera('Prof.Raja kumar')
+#enroll.enroll_via_camera('naveen')
+#enroll.enroll_via_camera('subhash')
+#enroll.enroll_via_camera('prasad')
